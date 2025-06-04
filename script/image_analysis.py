0ca447f97d6fc7160800b44afc84391103955a05
Vincent Verdillon <54115653+pulsar2105@users.noreply.github.com>
2025-06-04 18:51:19 +0200

    litlle things

--- script/image_analysis.py
+++ script/image_analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 import colorsys
 import os
 
@@ -37,14 +38,14 @@ def polar_to_cartesian(r, theta):
     return x, y
 
 
-path = "../Seizetheday/images/"
+path = "../images/"
 files = list_files(path)
 # import image only if it is a valid image
 images = [import_image(path + file) for file in files if file[-4:] != ".txt"]
 
-averages_shade = [main_shade(image) for image in images]
+averages_shade = [main_shade(image) for image in tqdm(images)]
 hsv = [rgb_to_hsv(average_shade) for average_shade in averages_shade]
 
-for i in range(len(images)):
+for i in tqdm(range(len(images))):
     with open(path + "hsv_" + files[i] + ".txt", "w+") as file:
         file.write(str(hsv[i][0]) + " " + str(hsv[i][1]) + " " + str(hsv[i][2]))
